9e65f3a1047cc6d90f3151914a74ac535551f5bd
Joy Nyayieka <67579434+JoyNyayieka@users.noreply.github.com>
2024-10-07 07:12:46 +0300

    Install required libraries and set up basic Flask app

---
+++ app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+# Create an instance of the Flask class
+app = Flask(__name__)
+
+# Define the home route for the web application
+@app.route('/')
+def home():
+    return "SafeWay App Running!"
+
+if __name__ == "__main__":
+    app.run(debug=True) 
