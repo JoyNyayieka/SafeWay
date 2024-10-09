1b5ed5d669b654208d419e897e23393c6724f0f2
Simon Kiragu <simonkiragu04@gmail.com>
2024-10-09 18:23:42 +0300

    Add front_end interface in the templates folder

--- app.py
+++ app.py
@@ -14,14 +14,15 @@ def routes():
     start_location = request.form['start_location']
     end_location = request.form['end_location']
 
-    # Process data, calculate safety score (replace with your logic)
-   
+    # Process data, calculate safety score 
 
-    return render_template('routes.html', start_location=start_location, end_location=end_location)  # Pass data to template
+    #Code after we make the model
+   #Fetch the model safety scores
+     # Pass data to template
+    return render_template('routes.html', start_location=start_location, end_location=end_location)  
 
 
-    # Handle the form submission here (e.g., process data, perform calculations)
-    # ... your code ...
+    
 
 if __name__ == "__main__":
     app.run(debug=True) 
