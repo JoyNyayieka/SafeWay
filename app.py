067a1613b9131e4c37b784342ec8e5a3e4b5f4c6
Simon Kiragu <simonkiragu04@gmail.com>
2024-10-09 16:23:30 +0300

    Add front_end interface in the templates

--- app.py
+++ app.py
@@ -1,12 +1,27 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 # Create an instance of the Flask class
 app = Flask(__name__)
 
 # Define the home route for the web application
 @app.route('/')
-def home():
-    return "SafeWay App Running!"
+def index():
+    return render_template('index.html')
+
+@app.route('/get_routes',  
+ methods=['POST'])
+def routes():  
+    start_location = request.form['start_location']
+    end_location = request.form['end_location']
+
+    # Process data, calculate safety score (replace with your logic)
+   
+
+    return render_template('routes.html', start_location=start_location, end_location=end_location)  # Pass data to template
+
+
+    # Handle the form submission here (e.g., process data, perform calculations)
+    # ... your code ...
 
 if __name__ == "__main__":
     app.run(debug=True) 
